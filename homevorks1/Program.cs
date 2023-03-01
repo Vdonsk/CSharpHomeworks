ce08f9a7967d607801ca8ca82d19e590c2a3ea0f
Андрей <vdon61@mail.ru>
2023-03-01 20:05:40 +0300

    Homework1_2

--- homevorks1/Program.cs
+++ homevorks1/Program.cs
@@ -5,24 +5,68 @@
 // a = -9; b = -3 -> max = -3
 
 
-int max = 0;
-int min = 0;
+// int max = 0;
+// int min = 0;
 
-Console.Write("a: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("b: ");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-if(a > b)
-{
-    max = a;
-    min = b;
-}
-else
-{
-    max = b;
-    min = a;
-}
-Console.WriteLine("max number ->" + max); 
-Console.WriteLine("min number ->" + min);
+// if(a > b)
+// {
+//     max = a;
+//     min = b;
+// }
+// else
+// {
+//     max = b;
+//     min = a;
+// }
+// Console.WriteLine("max number ->" + max); 
+// Console.WriteLine("min number ->" + min);
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// int max = 0;
+// Console.Write("first number: ");
+// int first = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("second number: ");
+// int second = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("third number: ");
+// int third = Convert.ToInt32(Console.ReadLine());
+
+// if (second > first)
+// {
+//     max = second;
+
+// if (third > second)
+// {
+//     max = third;
+// }
+// else
+// {
+//     max = second;
+// }
+// }
+// else
+// {
+//     max = first;
+// if (third > first)
+// {
+//     max = third;
+// }
+// else
+// {
+//     max = first;
+// }
+// }
+// Console.WriteLine("max number ->" + max);
