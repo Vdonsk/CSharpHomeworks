e9eb016c08888dfd1327859b1001cc3969b4ce97
Андрей <vdon61@mail.ru>
2023-03-01 21:23:40 +0300

    Homework1_3

--- homevorks1/Program.cs
+++ homevorks1/Program.cs
@@ -70,3 +70,23 @@
 // }
 // }
 // Console.WriteLine("max number ->" + max);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.Write("number: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int number = N % 2;
+
+// if(number == 0)
+// {
+//     Console.WriteLine("even");
+// }
+// else
+// {
+//     Console.WriteLine("not even");
+// }
