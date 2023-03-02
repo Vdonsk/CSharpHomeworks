e87b6885d2ee79fd9fdfbb37d60a26bd6728d09a
Андрей <vdon61@mail.ru>
2023-03-02 07:48:31 +0300

    Homework1_4

--- homevorks1/Program.cs
+++ homevorks1/Program.cs
@@ -34,6 +34,7 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+
 // int max = 0;
 // Console.Write("first number: ");
 // int first = Convert.ToInt32(Console.ReadLine());
@@ -71,11 +72,13 @@
 // }
 // Console.WriteLine("max number ->" + max);
 
+
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
 
 // Console.Write("number: ");
 // int N = Convert.ToInt32(Console.ReadLine());
@@ -90,3 +93,24 @@
 // {
 //     Console.WriteLine("not even");
 // }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Write("number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+if(n > 1)
+{
+    while(current <= n)
+    {
+        Console.Write(current + " ");
+        current = current + 2;
+    }
+}
