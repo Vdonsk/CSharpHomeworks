872b6801508302e53a380562734d85a1fc2d6ca4
Андрей <vdon61@mail.ru>
2023-03-16 19:37:57 +0300

    homework5_1

--- homework5/Program.cs
+++ homework5/Program.cs
@@ -2,40 +2,106 @@
 // [345, 897, 568, 234] -> 2
 
 
-Console.WriteLine("Input array size:");
-int numbers = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input minimal value of array element:");
-int minValue = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input maximal value of array element:");
-int maxValue = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateRandomArray(numbers, minValue, maxValue);
-ShowArray(myArray);
-int count = 0;
+// Console.WriteLine("Input the size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int count = 0;
 
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-    int[] newArray = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        newArray[i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return newArray;
-}
+// for (int i = 0; i < numbers.Length; i++)
+// if (numbers[i] % 2 == 0)
+// count++;
 
-void ShowArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
+// Console.WriteLine($"Total {numbers.Length} numbers, {count} of them even");
 
-int[] num = new int[numbers];
-{
-    for (int i = 0; i < num.Length; i++)
-        if (num[i] % 2 == 0)
-            count++;
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
 
-}
-Console.WriteLine($"Total {num.Length} numbers, {count} of them even");
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// Console.WriteLine("Input the size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int sum = 0;
+
+//     for (int i = 0; i < numbers.Length; i+=2)
+//         sum = sum + numbers[i];
+//         Console.WriteLine($"Sum of elements in odd positions :  {sum}");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+    
+//         numbers[i] = new Random().Next(-10,100);
+    
+// }
+// void PrintArray(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//              Console.WriteLine();
+// }
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+// Console.WriteLine("Input the size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double[] numbers = new double[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > max)
+        
+//             max = numbers[i];
+        
+//     if (numbers[i] < min)
+        
+//             min = numbers[i];
+        
+// }
+// Console.WriteLine($"Difference between the maximum and minimum : {max - min}");
+
+// void FillArrayRandomNumbers(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+        
+//             numbers[i] = Convert.ToDouble(new Random().Next(1,100));
+        
+// }
+// void PrintArray(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+        
+//             Console.Write(numbers[i] + " ");
+        
+//     Console.WriteLine();
+// }
