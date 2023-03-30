754d81a9a3cfb5e11a02f6d14e2f078ce86fa950
Андрей <vdon61@mail.ru>
2023-03-30 14:57:12 +0300

    homework9_3

--- homework9/Program.cs
+++ homework9/Program.cs
@@ -39,4 +39,24 @@
 // Console.Write("Input N: ");
 // int N = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine($"From M = {M} before N = {N} sum of natural elements = {FindSum(M, N)}");
+// Console.WriteLine($"From M = {M} before N = {N} -> sum of natural elements = {FindSum(M, N)}");
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+// int FunctionAkkermana(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return FunctionAkkermana(m - 1, 1);
+//     else return FunctionAkkermana(m - 1, FunctionAkkermana(m, n - 1));
+// }
+
+// Console.Write("Input M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"m = {m}, n = {n} -> Ackerman using recursion A(m,n) = {FunctionAkkermana(m, n)}");
